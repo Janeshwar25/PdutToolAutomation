33fafe476cd043e7f2cddca43c436da41ac9b4d3
Janeshwar25 <janeshwarchowdhary285@gmail.com>
2025-04-20 13:29:23 +0530

    Update RollBackPageTest.java

--- myproject/testcases/RollBackPageTest.java
+++ myproject/testcases/RollBackPageTest.java
@@ -6,26 +6,32 @@ import myproject.PageObjects.RollBackQueryPage;
 import myproject.QueryProcessor.BaseClass;
 import myproject.QueryProcessor.updatedSqlQuery;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import static org.testng.Assert.assertNotNull;
 
 public class RollBackPageTest extends BaseClass {
-
+    private static updatedSqlQuery sqlQueryProcessor;
     LoginPage loginPage;
     HomePage homePage;
     RollBackQueryPage rollBackQueryPage;
+
+    @BeforeClass
+    public static void collectInputOnce() {
+        sqlQueryProcessor = new updatedSqlQuery();
+        sqlQueryProcessor.collectUserInput();
+    }
+
+
     @BeforeMethod
     public void setup() {
 
         String browser = System.getProperty("browser","Edge");
         launchApp(browser);
-
+        loginPage = new LoginPage();
     }
 
-    @AfterMethod
-    public void tearDown() {
-        //getDriver().quit();
-    }
 
     @Test
     public void RollbackPageTest() throws Throwable {
@@ -36,8 +42,12 @@ public class RollBackPageTest extends BaseClass {
         HomePage homePage = loginPage.login(username,password);
         rollBack = homePage.clickonGenerateRollBack();
         rollBack.clickOnSelectApp();
-        rollBack.passDataInPastScriptArea();
+        String query = sqlQueryProcessor.getFirstQuery();
+        rollBack.passDataInPastScriptArea(query);
 
     }
-
+    @AfterMethod
+    public void tearDown() {
+        //getDriver().quit();
+    }
 }
