ca56128f227b94a3e3fc0f2cade50d26e8df4fa8
Janeshwar25 <janeshwarchowdhary285@gmail.com>
2025-04-20 13:27:31 +0530

    Update updatedSqlQuery.java
    new

--- myproject/QueryProcessor/updatedSqlQuery.java
+++ myproject/QueryProcessor/updatedSqlQuery.java
@@ -1,36 +1,18 @@
 package myproject.QueryProcessor;
-
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeTest;
-
 import java.util.Scanner;
 
 public class updatedSqlQuery extends BaseClass {
-    private String[] updatedQueries = new String[5];
+    private String[] updatedQueries ;
+    private static final Scanner SYSTEM_SCANNER = new Scanner(System.in);
+    private String planID;
+    private String effectiveDate;
+    private String benefitBundleID;
 
 
     public void collectUserInput() {
-        PageFactory.initElements(getDriver(), this);
-
-    }
-
-    public Object[][] getsqlQueries;
-
-    {
-        //access the Stored Sql Query
-        String sqlQuery1 = SqlQueryStorage.sqlQuery1;
-        String sqlQuery2 = SqlQueryStorage.sqlQuery2;
-        String sqlQuery3 = SqlQueryStorage.sqlQuery3;
-        String sqlQuery4 = SqlQueryStorage.sqlQuery4;
-        String sqlQuery5 = SqlQueryStorage.sqlQuery5;
-
-
-        //System.out.println(sqlQuery5);
-
         Scanner scanner = new Scanner(System.in);
-
-        //Accepting input for plan id and eff date from user
-
         System.out.print("Enter plan Id: ");
         String planID = scanner.nextLine();
         System.out.print("Enter effectiveDate(YYYY-MM-DD): ");
@@ -40,21 +22,30 @@ public class updatedSqlQuery extends BaseClass {
         System.out.print("Enter benefitBundleId(optional, press Enter to skip):");
         String benefitBundleID = scanner.nextLine();
 
+        updatedQueries ();
+    }
 
-        // Replace planId and effective in each query
+    //access the Stored Sql Query
+    private void updatedQueries() {
+        updatedQueries[0] = SqlQueryStorage.sqlQuery1.replace("2018-01-01", effectiveDate).replace("RX01000085", planID);
+        updatedQueries[1] = SqlQueryStorage.sqlQuery2.replace("2018-01-01", effectiveDate).replace("RX01000085", planID);
+        updatedQueries[2] = SqlQueryStorage.sqlQuery3.replace("2018-01-01", effectiveDate).replace("RX01000085", planID);
+        updatedQueries[3] = SqlQueryStorage.sqlQuery4.replace("2018-01-01", effectiveDate).replace("RX01000085", planID);
+        updatedQueries[4] = SqlQueryStorage.sqlQuery5.replace("2018-01-01", effectiveDate).replace("RX01000085", planID);
 
-        String updatedSqlQuery1 = sqlQuery1.replace("2018-01-01", effectiveDate).replace("RX01000085", planID);
+
+       /* String updatedSqlQuery1 = sqlQuery1.replace("2018-01-01", effectiveDate).replace("RX01000085", planID);
         String updatedSqlQuery2 = sqlQuery2.replace("2018-01-01", effectiveDate).replace("RX01000085", planID);
         String updatedSqlQuery3 = sqlQuery3.replace("2018-01-01", effectiveDate).replace("RX01000085", planID);
         String updatedSqlQuery4 = sqlQuery4.replace("2018-01-01", effectiveDate).replace("RX01000085", planID);
 
         //Optional benefitBundleId in sqlQuery5
-        String updatedSqlQuery5 = sqlQuery5.replace("2018-01-01", effectiveDate).replace("RX01000085", planID);
+        String updatedSqlQuery5 = sqlQuery5.replace("2018-01-01", effectiveDate).replace("RX01000085", planID);*/
 
         if (!benefitBundleID.isEmpty()) {
-            updatedSqlQuery5 = updatedSqlQuery5.replace("51765352", benefitBundleID);
+            updatedQueries[4] = updatedQueries[4].replace("51765352", benefitBundleID);
         } else {
-            updatedSqlQuery5 = updatedSqlQuery5.replace("{benefitBundleId}", "");
+            updatedQueries[4] = updatedQueries[4].replace("{benefitBundleId}", "");
         }
 
         /*System.out.println(updatedSqlQuery1);
@@ -65,12 +56,9 @@ public class updatedSqlQuery extends BaseClass {
 
     }
 
-
-        public String[] getUpdatedQueries () {
-        return this.updatedQueries;
+    public String getFirstQuery(){
+        return updatedQueries[0];
     }
 
-        public String getQuery ( int index){
-        return updatedQueries[index];
-    }
+
     }
