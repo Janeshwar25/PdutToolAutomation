fc291bf44b0872325d96de1896e1476b7fc9f238
Janeshwar25 <janeshwarchowdhary285@gmail.com>
2025-04-21 18:37:33 +0530

    Update updatedSqlQuery.java

--- QueryProcessor/updatedSqlQuery.java
+++ QueryProcessor/updatedSqlQuery.java
@@ -1,10 +1,16 @@
 package myproject.QueryProcessor;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeTest;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class updatedSqlQuery extends BaseClass {
-    private String[] updatedQueries ;
+    private String[] updatedQueries = new String[5];
     private static final Scanner SYSTEM_SCANNER = new Scanner(System.in);
     private String planID;
     private String effectiveDate;
@@ -12,15 +18,22 @@ public class updatedSqlQuery extends BaseClass {
 
 
     public void collectUserInput() {
-        Scanner scanner = new Scanner(System.in);
+
+
+        //Scanner scanner = new Scanner(System.in);
         System.out.print("Enter plan Id: ");
-        String planID = scanner.nextLine();
+        planID = SYSTEM_SCANNER.nextLine();
+
+        //String planID = scanner.nextLine();
         System.out.print("Enter effectiveDate(YYYY-MM-DD): ");
-        String effectiveDate = scanner.nextLine();
+        effectiveDate = SYSTEM_SCANNER.nextLine();
+
+        //String effectiveDate = scanner.nextLine();
 
         // Optional input for benefitBundleId
         System.out.print("Enter benefitBundleId(optional, press Enter to skip):");
-        String benefitBundleID = scanner.nextLine();
+        benefitBundleID = SYSTEM_SCANNER.nextLine();
+        //String benefitBundleID = scanner.nextLine();
 
         updatedQueries ();
     }
@@ -60,5 +73,59 @@ public class updatedSqlQuery extends BaseClass {
         return updatedQueries[0];
     }
 
+    public String getSecondQuery(){
+        return updatedQueries[1];
+    }
+
+    public String getThirdQuery(){
+        return updatedQueries[2];
+    }
+
+    public String getFourthQuery(){
+        return updatedQueries[3];
+    }
+
+    public String getFifthQuery(){
+        return updatedQueries[4];
+    }
+
+    public String getBenefitBundleID(){
+        return this.benefitBundleID;
+    }
+
+    public String[] getAllQueries(){
+        return updatedQueries;
+    }
+
+    public class QueryPair{
+        private String rollbackQuery;
+        private String sqlQuery;
+
+        public QueryPair(String rollbackQuery, String sqlQuery){
+            this.rollbackQuery = rollbackQuery;
+            this.sqlQuery = sqlQuery;
+
+        }
+
+        public String getRollbackQuery(){
+            return rollbackQuery;
+        }
+        public String getSqlQuery(){
+            return sqlQuery;
+        }
+
+        public List<QueryPair> getAllQueryPair(){
+            List<QueryPair> pairs = new ArrayList<>();
+            pairs.add(new QueryPair(rollbackQuery1, sqlQuery1));
+            pairs.add(new QueryPair(rollbackQuery2, sqlQuery2));
+            pairs.add(new QueryPair(rollbackQuery3, sqlQuery3));
+            pairs.add(new QueryPair(rollbackQuery4, sqlQuery4));
+            pairs.add(new QueryPair(rollbackQuery5, sqlQuery5));
+
+            return pairs;
+
+
+        }
+    }
 
     }
