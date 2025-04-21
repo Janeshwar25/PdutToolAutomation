c76da6d991c58a5a60b5ae56f6c040175fe04cae
Janeshwar25 <janeshwarchowdhary285@gmail.com>
2025-04-21 14:18:18 +0530

    Update RollBackPageTest.java

--- testcases/RollBackPageTest.java
+++ testcases/RollBackPageTest.java
@@ -1,53 +1,104 @@
 package myproject.testcases;
 
-import myproject.PageObjects.HomePage;
+import myproject.PageObjects.*;
 import myproject.PageObjects.LoginPage;
 import myproject.PageObjects.RollBackQueryPage;
 import myproject.QueryProcessor.BaseClass;
-import myproject.QueryProcessor.updatedSqlQuery;
-import org.testng.annotations.AfterMethod;
+import myproject.QueryProcessor.*;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import static org.testng.Assert.assertNotNull;
 
 public class RollBackPageTest extends BaseClass {
     private static updatedSqlQuery sqlQueryProcessor;
-    LoginPage loginPage;
-    HomePage homePage;
-    RollBackQueryPage rollBackQueryPage;
+    private static LoginPage loginPage;
+    private static HomePage homePage;
+    private RollBackQueryPage rollBackQueryPage;
+    private static WebDriver driver;
+    private static Properties prop = new Properties();
 
-    @BeforeClass
+  /*  @BeforeSuite
     public static void collectInputOnce() {
         sqlQueryProcessor = new updatedSqlQuery();
         sqlQueryProcessor.collectUserInput();
-    }
+    }*/
 
+    public static void main(String[] args) throws Throwable {
 
-    @BeforeMethod
-    public void setup() {
+        updatedSqlQuery sqlQueryProcessor = new updatedSqlQuery();
+        sqlQueryProcessor.collectUserInput();
 
-        String browser = System.getProperty("browser","Edge");
-        launchApp(browser);
-        loginPage = new LoginPage();
-    }
+        /*BaseClass base = new BaseClass();
+        base.loadConfig();
+        base.launchApp("Edge");
 
+        WebDriver driver = BaseClass.getDriver();
 
-    @Test
-    public void RollbackPageTest() throws Throwable {
         LoginPage loginPage = new LoginPage();
-        RollBackQueryPage rollBack = new RollBackQueryPage();
         String username = prop.getProperty("username");
         String password = prop.getProperty("password");
-        HomePage homePage = loginPage.login(username,password);
-        rollBack = homePage.clickonGenerateRollBack();
+        HomePage homePage = loginPage.login(username, password);
+
+        RollBackQueryPage rollBack = homePage.clickonGenerateRollBack();
         rollBack.clickOnSelectApp();
         String query = sqlQueryProcessor.getFirstQuery();
         rollBack.passDataInPastScriptArea(query);
 
+        System.out.println("RollBack test completed");*/
+
+        RollBackPageTest runner = new RollBackPageTest();
+        runner.runRollBackPageTest(sqlQueryProcessor);
     }
-    @AfterMethod
-    public void tearDown() {
-        //getDriver().quit();
+
+    public void runRollBackPageTest(updatedSqlQuery sqlQueryProcessor) throws Throwable {
+
+        loadConfig();
+        String browser = System.getProperty("browser", "Edge");
+        launchApp(browser);
+
+        LoginPage loginPage = new LoginPage();
+        String username = BaseClass.prop.getProperty("username");
+        String password = BaseClass.prop.getProperty("password");
+        HomePage homePage = loginPage.login(username, password);
+
+        RollBackQueryPage rollBack = homePage.clickonGenerateRollBack();
+        rollBack.clickOnSelectApp();
+        String query = sqlQueryProcessor.getFirstQuery();
+        rollBack.passDataInPastScriptArea(query);
+        rollBack.ClickonGenerateRollBack();
+        rollBack.taketextfromRollBackTextArea1();
+
+        rollBack.ClickonResetButton();
+
+        rollBack.clickOnSelectApp();
+        String query1 = sqlQueryProcessor.getSecondQuery();
+        rollBack.passDataInPastScriptArea(query1);
+        /*rollBack.ClickonGenerateRollBack();
+        rollBack.taketextfromRollBackTextArea2();
+
+        rollBack.ClickonResetButton();*/
+
+
+
+
+
+
+
+
+
+        System.out.println("RollBack test completed");
+
     }
+
+
 }
